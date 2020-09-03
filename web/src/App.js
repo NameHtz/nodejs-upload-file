fc088d47f992dd5504e521e6e06b879a9ff0acc3
NameHtz <breakname@163.com>
2020-09-03 15:21:41 +0800

    React + antd

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import UploadImage from "./pages/uploadImage";
+import './App.css';
+
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UploadImage/>
+    </div>
+  );
+}
+
+export default App;
